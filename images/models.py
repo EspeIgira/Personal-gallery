d7add4c74f3c8c479dd5ef8a01e15a48b0ae326e
EspeIgira <Mapapuro2>
2019-03-15 09:13:11 +0200

    first class created

--- images/models.py
+++ images/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Welcome(models.Model):
+
+    name = models.CharField(max_length =30)
+    welcome_Main_Img = models.ImageField(upload_to='images/') 
+
+    def __str__(self):
+        return self.name
+
+
+    def save_editor(self):
+        self.save()
+
+
+    def delete_editor(self):
+        self.delete()
+
+    def update_editor(self):
+        self.update()
+    
+    def display_editor(self):
+        self.display()
+
+    class Meta:
+        ordering = ['name']
