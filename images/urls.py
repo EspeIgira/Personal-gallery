a6655d6428c99cd164ad2e9e49d88fdaeb0095af
EspeIgira <Mapapuro2>
2019-03-15 09:01:48 +0200

    a configuration link

--- images/urls.py
+++ images/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.gallery,name = 'gallery'),
-    
-   
+    url('^$',views.gallery,name = 'gallery'),  
 ]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
